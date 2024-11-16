614173d435d57cdddaac8bf2381aadaf1bcee3b7
Subhadeep Choudhury <81243263+devSubho51347@users.noreply.github.com>
2024-11-17 04:56:52 +0530

    Fine tuned notebook for LLama 1b model

--- Streamlit_App/Chatbot.py
+++ Streamlit_App/Chatbot.py
@@ -1,20 +1,40 @@
 
 import streamlit as st
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
-import torch
-from huggingface_hub import login
+# from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+# import torch
+# from huggingface_hub import login
+#
+# import pandas as pd
+# import torch
+# import re
+# import huggingface_hub
+# from datasets import Dataset
+# import transformers
+# from transformers import (
+#     BitsAndBytesConfig,
+#     AutoModelForCausalLM,
+#     AutoTokenizer,
+# )
 
-import pandas as pd
-import torch
-import re
-import huggingface_hub
-from datasets import Dataset
-import transformers
-from transformers import (
-    BitsAndBytesConfig,
-    AutoModelForCausalLM,
-    AutoTokenizer,
-)
+from openai import OpenAI
+from time import sleep
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from the secrets file
+load_dotenv('D:\Python_projects\Anime-Chatbot\secrets.env')
+
+# Access the API key
+openai_api_key = os.getenv('OPENAI_API_KEY')
+# print(openai_api_key)
+
+# Initialize OpenAI client
+# client = OpenAI(api_key = openai_api_key)
+client = OpenAI(api_key= openai_api_key)
+
+job = client.fine_tuning.jobs.retrieve("ftjob-y8L4VubWdpixj580bmko1jzE")
+my_model = job.fine_tuned_model
+
 
 from dummy import load_finetuned_model
 from peft import LoraConfig, PeftModel
@@ -28,6 +48,20 @@ def app():
     # Replace 'YOUR_TOKEN' with your actual Hugging Face Hub token.
     # You can generate a token at https://huggingface.co/settings/tokens
     # login(token='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')
+
+    def test_model(model_id, test_input):
+        """Test the fine-tuned model"""
+        completion = client.chat.completions.create(
+            model=model_id,
+            messages=[
+                {
+                    "role": "system",
+                    "content": "Your are Naruto from the anime 'Naruto'. Your responses should reflect his personality and speech patterns. All your responses should have a lot of energy and enthusiasm"
+                },
+                {"role": "user", "content": test_input}
+            ]
+        )
+        return completion.choices[0].message
 
     if "messages" not in st.session_state:
         st.session_state["messages"] = []
@@ -52,14 +86,14 @@ def app():
     local_directory = "../local_model_directory"
 
     # Load the model and tokenizer from the local directory
-    model = AutoModelForCausalLM.from_pretrained(local_directory)
-    tokenizer = AutoTokenizer.from_pretrained(local_directory,use_fast=False)
-
-    tokenizer.chat_template = "<|system|>{system}\n<|user|>{user}\n<|assistant|>{assistant}"
-
-    # Set up a pipeline for text generation
-    chat = pipeline("text-generation", model=model, tokenizer=tokenizer)
-    # chat = load_finetuned_model()
+    # model = AutoModelForCausalLM.from_pretrained(local_directory)
+    # tokenizer = AutoTokenizer.from_pretrained(local_directory,use_fast=False)
+    #
+    # tokenizer.chat_template = "<|system|>{system}\n<|user|>{user}\n<|assistant|>{assistant}"
+    #
+    # # Set up a pipeline for text generation
+    # chat = pipeline("text-generation", model=model, tokenizer=tokenizer)
+    # # chat = load_finetuned_model()
 
     # System message (optional, you can customize or remove it)
     system_prompt = "Your are Naruto from the anime 'Naruto'. Your responses should reflect his personality and speech patterns"
@@ -103,11 +137,12 @@ def app():
 
     if user_input:
         # Format input using the chat template
-        formatted_input = tokenizer.chat_template.format(system=system_prompt, user=user_input, assistant="")
+        # formatted_input = tokenizer.chat_template.format(system=system_prompt, user=user_input, assistant="")
 
         # Generate model response
-        response = chat(formatted_input, max_length=100, do_sample=True)
-        assistant_response = response[0]["generated_text"].split("<|assistant|>")[-1].strip()
+        # response = chat(formatted_input, max_length=100, do_sample=True)
+        assistant_response = test_model(my_model,user_input)
+        assistant_response = assistant_response.content
 
         # Append user and assistant messages to session state for display
         st.session_state["messages"].append({"role": "user", "content": user_input})
